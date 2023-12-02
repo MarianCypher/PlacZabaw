b7b27c3a12f87c255b8bfb5276f959af95395ff4
MarianCypher <p.rybczynski@hotmail.com>
2023-12-02 14:59:21 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Zadanko polega na odwzorowaniu placu zabaw za pomocą zestawu klas
+ *
+ * uwaga: każda klasa, interfejs, klasa abstrakcyjna powinna zostać utworzona w nowym pliku;
+ * wszystkie zmienne, klasy, interfejsy mają nazywać się po angielsku
+ */
+
