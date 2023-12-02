e598b2f2de6f558b1d024b2ca732f1cb77836d25
MarianCypher <p.rybczynski@hotmail.com>
2023-12-02 16:39:15 +0100

    Interfaces addition

---
+++ IPlaygroundEnjoyer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public interface IPlaygroundEnjoyer
+{   
+    // personal data
+    string Name { get; set; }
+    string Age { get; set; }
+    string Gender { get; set; }
+    string Height { get; set; }
+    string Weight { get; set; }
+    string Happiness { get; set; }
+    string Monay { get; set; }
+
+    // side effects
+    bool Queue();
+    bool Play();
+    bool Pay();
+    bool Accident();
+    bool PlayExtension();
+}
---
+++ IWeightable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface IWeightable
+{
+    public double GetWeight {
+        get;
+        set;
+    }
+}
