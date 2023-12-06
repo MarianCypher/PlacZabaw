fc0bce12ac953b4daf11e8b7a460a947559b5e44
MarianCypher <p.rybczynski@hotmail.com>
2023-12-06 13:03:25 +0100

    Classes of routes and users introduced

---
+++ Kido.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace PlacZabaw
+{
+    class Kido: IPlaygroundEnjoyer, IWeightable
+    {
+        //Properties
+        public double GetWeight { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public double Happiness { get; set; }
+        public double Money { get; set; }
+        public int Queue { get; set; }
+        public bool Playing { get; set; }
+        public bool Accident { get; set; }
+        public bool PlayExtension { get; set; }
+
+        //Constructor
+        public Kido(double getWeight, string name, int age, string gender, double height, double weight, double happiness, double money, int queue, bool playing = false, bool accident = false, bool playExtension = false)
+        {
+            GetWeight = getWeight;
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Height = height;
+            Weight = weight;
+            Happiness = happiness;
+            Money = money;
+            Queue = queue;
+            Playing = playing;
+            Accident = accident;
+            PlayExtension = playExtension;
+        }
+
+        //Metody z interfejsow
+        public int QueueStatus(int position)
+        {
+            return Queue + position;
+        }
+        public string Play(string rideName, double price)
+        {
+            if (rideName == "stop" || rideName == "none")
+            {
+                Queue = 0;
+                Playing = false;
+            }
+            else
+            {
+                if (price > Money)
+                {
+                    Queue = 0;
+                    Playing = false;
+                    return $"Sorry its out of Your budget.";
+                }
+                else
+                {
+                    Playing = true;
+                    return $"{Name} is playing on {rideName}";
+                    // uzyc tutaj pozostalych metod w metodzie? w koncu zabawa na atrakcji to tez koszt tez ryzyko urazu itp
+                }  
+            }
+        }
+        public double Pay(double amount)
+        {
+            Money -= amount;
+            return Money;
+        }
+        public bool AccidentTrigger()
+        {
+            Random random = new Random();
+            int min = 1;
+            int max = 100;
+            int randomValue = random.Next(min, max+1);
+            if (randomValue < 10)
+            {
+                Accident = true;
+            }
+        }
+        public bool PlayExtensionTrigger()
+        {
+            Random random = new Random();
+            int min = 1;
+            int max = 100;
+            int randomValue = random.Next(min, max + 1);
+            if (randomValue < 10)
+            {
+                PlayExtension = true;
+            }
+        }
+    }
+}
---
+++ Sandbox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PlacZabaw
+{
+    class Sandbox: IPlayable, IWeightable
+    {
+        //Properties
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public double Duration { get; set; }
+        public double GetWeight { get; set; }
+ 
+        //Constructor
+        public Sandbox(string name, double cost, double duration)
+        {
+            Name = name;
+            Cost = cost;
+            Duration = duration;
+        }
+
+        //Metody z interfejsow
+        public virtual bool Play()
+        {
+            return $"This {Name} is used right now.";
+        }
+    }
+}
